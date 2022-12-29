6a73365d3b456dcc912521e14850df9c42ecd4c3
PavloZiuskin <passzuskin@gmail.com>
2022-12-29 21:15:30 +0200

    add-complit-task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let intervalId = null;
 
+refs.btnStart.addEventListener('click', onStartChengeBodyColorSetIntervalClick);
+refs.btnStop.addEventListener('click', onStopRandomColorIntervalBtnStopClick);
+
+function onStartChengeBodyColorSetIntervalClick() {
+  refs.btnStart.disabled = true;
+  intervalId = setInterval(() => {
+    return (refs.body.style.backgroundColor = getRandomHexColor());
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStopRandomColorIntervalBtnStopClick() {
+  refs.btnStart.disabled = false;
+  clearInterval(intervalId);
+}
