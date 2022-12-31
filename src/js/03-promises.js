bb37dfefccc3cf754aa8dc947050ea306d73a288
PavloZiuskin <passzuskin@gmail.com>
2022-12-31 16:15:26 +0200

    add-complited-task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+const refs = {
+  form: document.querySelector('.form'),
+};
+refs.form.addEventListener('submit', onCreateAmountPromiseFormSubmit);
+
+let delay = 0;
+let step = 0;
+let amount = 0;
+let position = 0;
+
+function onCreateAmountPromiseFormSubmit(e) {
+  e.preventDefault();
+  const elementOfInput = e.target.elements;
+  delay = Number(elementOfInput.delay.value);
+  step = Number(elementOfInput.step.value);
+  amount = Number(elementOfInput.amount.value);
+  console.log(delay, step, amount);
+  delay -= step;
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, (delay += step))
+      .then(resp => console.log(resp))
+      .catch(resp => console.log(resp));
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        res(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        rej(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+  return promise;
 }
